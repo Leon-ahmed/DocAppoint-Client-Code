1fc6c3f026107aa9a65ae1cdd7da6a895c27fb0c
Leon-ahmed <mdleon1200@gmail.com>
2026-09-25 21:07:50 +0600

    profile code reorganized

--- src/app/dashboard/profile/page.jsx
+++ src/app/dashboard/profile/page.jsx
@@ -1,41 +1,18 @@
-"use client";
+ export const metadata = {
+  title: " DocAppoint | My Profile ",
+  description: " Profile",
+};
 
-import { useSession } from "@/lib/auth-client";
-import { Avatar, Button } from "@heroui/react";
+import Profile from "@/components/Profile";
+
+
+const UserProfile = () => {
+ 
+
 
  
 
-const UserProfile = () => {
-      const { data: session, isPending } = useSession();
-  const user = session?.user;
-  return (
-    <div className="   flex justify-center items-center  rounded-xl border bg-linear-to-br from-[#c9c9c9] to-[#E7F6F3] p-6 shadow-sm m-5">
-      <div className="flex flex-col items-center text-center">
-        
-        <Avatar className="h-24 w-24">
-          <Avatar.Image
-            alt={user?.name || "User"}
-            src={user?.image}
-          />
-          <Avatar.Fallback>
-            {user?.name?.[0] || "U"}
-          </Avatar.Fallback>
-        </Avatar>
-
-        <h2 className="mt-4 text-xl font-semibold">
-          {user?.name || "User"}
-        </h2>
-
-        <p className="mt-1 text-sm text-gray-500">
-          {user?.email || "No email available"}
-        </p>
-
-        <Button className="mt-5 bg-[#00685F] text-white">
-          Edit Profile
-        </Button>
-      </div>
-    </div>
-  );
+  return <Profile  />;
 };
 
 export default UserProfile;
